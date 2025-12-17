f6c76e2a9388644ea7a67190b5b4638feda92ea9
Mai Vang <vmaineng@gmail.com>
2025-12-16 17:16:50 -0800

    added in error message

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Input } from "../src/ui/input";
 import { Label } from "../src/ui/label";
 import { Button } from "../src/ui/button";
@@ -18,7 +18,7 @@ import { useRouter } from "next/navigation";
 
 const formSignupSchema = z
   .object({
-    email: z.string().email(),
+    email: z.email(),
     password: z.string().min(8),
     confirmPassword: z.string().min(8),
   })
@@ -30,11 +30,67 @@ const formSignupSchema = z
 type FormSignUpValues = z.infer<typeof formSignupSchema>;
 
 export function Signup() {
+  const router = useRouter();
+  const [error, setError] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [showSuccessModal, setShowSuccessModal] = useState(false);
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const name = formData.get;
+  const [redirectCountdown, setRedirectCountdown] = useState(5);
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [showConfirmPassword, setShowConfirmPassword] =
+    useState<boolean>(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormSignUpValues>({
+    resolver: zodResolver(formSignupSchema),
+  });
+
+  useEffect(() => {
+    if (!showSuccessModal) return;
+
+    const timer = setInterval(() => {
+      setRedirectCountdown((prev) => {
+        if (prev <= 1) {
+          clearInterval(timer);
+          router.push("/note");
+          return 0;
+        }
+        return prev - 1;
+      });
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [showSuccessModal, router]);
+
+  const handleRegister = async (data: FormSignUpValues) => {
+    setIsLoading(true);
+    setError("");
+    try {
+      const { error } = await signUpNewUser(data.email, data.password);
+      if (error) {
+        throw error;
+      } else {
+        setShowSuccessModal(true);
+        setRedirectCountdown(2);
+        router.push("/note");
+      }
+    } catch (error) {
+      let errorMessage = "Signup failed. Please try again.";
+
+      if (error instanceof Error) {
+        if (error.message.toLowerCase().includes("user already registered")) {
+          errorMessage = "Account already exists. Please login instead.";
+        } else {
+          errorMessage = error.message;
+        }
+      } else if (typeof error === "string") {
+        errorMessage = error;
+      }
+      setError(errorMessage);
+    } finally {
+      setIsLoading(false);
+    }
   };
 }
